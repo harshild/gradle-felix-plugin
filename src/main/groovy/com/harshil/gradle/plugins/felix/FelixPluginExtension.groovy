75c5b1408c698fbffdb64139f707fe20e48b7258
Harshild <harshil.dhariwal@gmail.com>
2016-04-02 03:32:53 +0530

    Upgrade Felix Version to 5.4.0

--- src/main/groovy/com/harshil/gradle/plugins/felix/FelixPluginExtension.groovy
+++ src/main/groovy/com/harshil/gradle/plugins/felix/FelixPluginExtension.groovy
@@ -5,7 +5,7 @@ class FelixPluginExtension {
     String deployActions = 'install,start,update'
     int httpPort = 8080
     String repositoryUrl = 'http://felix.apache.org/obr/releases.xml'
-    String mainArtifact = 'org.apache.felix:org.apache.felix.main:4.0.2'
+    String mainArtifact = 'org.apache.felix:org.apache.felix.main:5.4.0'
     
     Map properties = [:]
     
