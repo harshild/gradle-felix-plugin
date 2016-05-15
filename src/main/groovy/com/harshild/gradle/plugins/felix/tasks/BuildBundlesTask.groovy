79862340531c10eb1d8eba079d1c704106cf2193
Harshild <harshil.dhariwal@gmail.com>
2016-05-15 14:35:19 +0530

    user can exclude subproject from felix build. It will marked as dependency project

--- src/main/groovy/com/harshild/gradle/plugins/felix/tasks/BuildBundlesTask.groovy
+++ src/main/groovy/com/harshild/gradle/plugins/felix/tasks/BuildBundlesTask.groovy
@@ -2,11 +2,19 @@ package com.harshild.gradle.plugins.felix.tasks
 
 import com.harshild.gradle.plugins.felix.util.BndWrapper
 import com.harshild.gradle.plugins.felix.util.BundleUtils
+import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.artifacts.Dependency
 
+import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
+import java.util.zip.ZipOutputStream
+
 
 class BuildBundlesTask extends BaseTask {
+
+    final String OSGI_CORE = "org.osgi.core-4.2.0.jar"
+    final String COMMONS_LANG = "commons-lang3-3.1.jar"
     final String CONFIG_TEMPLATE = """felix.log.level=%d
 felix.auto.deploy.action=%s
 org.osgi.service.http.port=%d
@@ -17,24 +25,67 @@ obr.repository.url=%s
     def String jar(Dependency dep) {
         return "${dep.name}-${dep.version}.jar"
     }
-    
+
     def bundleProjects(rootProject) {
-        rootProject.subprojects.findAll { project -> project.name != name }
-    }	
-    
-    def copySubprojects(rootProject, target) {
-        bundleProjects(rootProject).each { project ->
-            ant.copy(file: "${project.buildDir.absolutePath}/libs/${project.name}-${project.version}.jar",
-                todir: "$target")
+        List<String> excludeP = new ArrayList<>();
+        Set<Project> bundles = new ArrayList<>();
+        project.extensions.felix.excludedProjects.each{
+            excludeP.add(it);
         }
-    }
-    
-    def copySubprojectsConfigResource(rootProject, target) {
-        bundleProjects(rootProject).each { project ->
-        if(new File("${project.buildDir.absolutePath}/../config/").exists())
-            ant.copy(todir: "$target/../config"){
-            	fileset(dir: "${project.buildDir.absolutePath}/../config/")
+
+        rootProject.subprojects.each {project ->
+            excludeP.each { ex ->
+                if(project.name != ex && project.name != name)
+                    bundles.add(project)
             }
+        }
+        bundles
+    }
+
+    def copySubProjects(rootProject, target) {
+        List<String> filterDep = new ArrayList<>()
+        filterDep.add(OSGI_CORE)
+        filterDep.add(COMMONS_LANG)
+        bundleProjects(rootProject).forEach{project->
+            String temp = project.name+"-"+project.version+".jar"
+            filterDep.add(temp)
+        }
+        bundleProjects(rootProject).each { project ->
+            List<File> fileList = new ArrayList<>();
+            project.configurations.compile.each { z ->
+                if (!filterDep.contains(z.name)) {
+                    fileList.add(new File(z.toString()))
+                }
+            }
+            File baseProject = new File("${project.buildDir.absolutePath}/libs/${project.name}-${project.version}.jar")
+            fileList.add(baseProject)
+
+            def bundle = new File( "$target/${project.name}-${project.version}.jar" )
+
+            BundleUtils.fatJar( fileList, bundle ) {
+                ZipFile input, ZipOutputStream out, ZipEntry entry ->
+                    String temp = project.name+"-"+project.version+".jar"
+                    if ( (input.name.contains(temp) ||
+                            (entry.name != 'META-INF/MANIFEST.MF' &&
+                                    entry.name != 'OSGI-INF/serviceComponents.xml')
+                    ) &&
+                            !entry.isDirectory()) {
+                        out.putNextEntry(entry)
+                        out.write(input.getInputStream(entry).bytes)
+                    }
+
+            }
+
+        }
+
+    }
+
+    def copySubProjectsConfigResource(rootProject, target) {
+        bundleProjects(rootProject).each { project ->
+            if(new File("${project.buildDir.absolutePath}/../config/").exists())
+                ant.copy(todir: "$target/../config"){
+                    fileset(dir: "${project.buildDir.absolutePath}/../config/")
+                }
         }
     }
 
@@ -60,28 +111,26 @@ obr.repository.url=%s
         }
 
         nonBundles.each { File file ->
-            println(file.name)
-
-                try {
-                    BndWrapper.wrapNonBundle( file, bundleDir )
-                } catch ( e ) {
-                    println( "Unable to wrap ${file.name}" + e.message )
-                }
+            try {
+                BndWrapper.wrapNonBundle( file, bundleDir )
+            } catch ( e ) {
+                println( "Unable to wrap ${file.name}" + e.message )
+            }
 
         }
 
         def confDir = "$targetDir/conf"
         new File(confDir).mkdirs()
         new File("$confDir/config.properties").withWriter { w ->
-          w.write(String.format(CONFIG_TEMPLATE,
-                  project.extensions.felix.logLevel,
-                  project.extensions.felix.deployActions,
-                  project.extensions.felix.httpPort,
-                  project.extensions.felix.repositoryUrl,
-                  project.extensions.felix.propertiesString
-          ))
+            w.write(String.format(CONFIG_TEMPLATE,
+                    project.extensions.felix.logLevel,
+                    project.extensions.felix.deployActions,
+                    project.extensions.felix.httpPort,
+                    project.extensions.felix.repositoryUrl,
+                    project.extensions.felix.propertiesString
+            ))
         }
-        copySubprojects(project, bundleDir)
-        copySubprojectsConfigResource(project, bundleDir)
+        copySubProjects(project, bundleDir)
+        copySubProjectsConfigResource(project, bundleDir)
     }
 }
