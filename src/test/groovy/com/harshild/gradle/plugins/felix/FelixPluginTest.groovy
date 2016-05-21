2ba34f0a965827c59d2c97ff723080d8c16ca4fd
Harshild <harshil.dhariwal@gmail.com>
2016-05-21 15:38:36 +0530

    bug fix exclude project

--- src/test/groovy/com/harshild/gradle/plugins/felix/FelixPluginTest.groovy
+++ src/test/groovy/com/harshild/gradle/plugins/felix/FelixPluginTest.groovy
@@ -1,6 +1,7 @@
 package com.harshild.gradle.plugins.felix
 
 import org.junit.Test
+import org.junit.Before
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import com.harshild.gradle.plugins.felix.tasks.BuildBundlesTask
@@ -8,10 +9,16 @@ import com.harshild.gradle.plugins.felix.tasks.RunFelixTask
 import static org.testng.Assert.*
 
 class FelixPluginTest {
+Project project ;
+@Before
+public void init(){
+project = ProjectBuilder.builder().build()
+    project.apply plugin: 'com.harshild.felix-run'
+}
+
     @Test
     public void itShouldAssFelixExtension() {
-        Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'com.harshild.felix-run'
+
 
         assertTrue(project.extensions.felix instanceof FelixPluginExtension)
 
@@ -19,8 +26,6 @@ class FelixPluginTest {
 
     @Test
     public void itShouldActiveTasks() {
-        Project project = ProjectBuilder.builder().build()
-        project.apply plugin: 'com.harshild.felix-run'
 
         assertTrue(project.tasks.buildBundles instanceof BuildBundlesTask)
         assertTrue(project.tasks.runFelix instanceof RunFelixTask)
