f7a0a76a37010c41831266ba03d26976156f1bda
Harshild <harshil.dhariwal@gmail.com>
2016-06-10 09:34:13 +0530

    Code for version parsing

--- src/main/groovy/com/harshild/gradle/plugins/felix/util/BndWrapper.groovy
+++ src/main/groovy/com/harshild/gradle/plugins/felix/util/BndWrapper.groovy
@@ -3,6 +3,7 @@ package com.harshild.gradle.plugins.felix.util
 
 import aQute.bnd.osgi.Analyzer
 import aQute.bnd.osgi.Jar
+import aQute.bnd.version.Version
 
 import java.util.jar.Manifest
 import java.util.zip.ZipEntry
@@ -19,8 +20,8 @@ class BndWrapper {
         def currentManifest = newJar.manifest ?: new Manifest()
 
 
-        String implVersion =currentManifest.mainAttributes.getValue( 'Implementation-Version' ) ?:
-                        versionFromFileName( jarFile.name )
+        String implVersion =getVersion(currentManifest.mainAttributes.getValue( 'Implementation-Version' ) ?:
+                        versionFromFileName( jarFile.name ))
 
         String implTitle = currentManifest.mainAttributes.getValue( 'Implementation-Title' ) ?:
                         titleFromFileName( jarFile.name )
@@ -64,8 +65,29 @@ class BndWrapper {
 
     }
 
+    static String getVersion(String version) {
+        String newVersion = "";
+        int releaseType = 1;
+        version.toCharArray().each {
+            if((it>47 && it <58))
+                newVersion = newVersion+it;
+            if(it == 46 && releaseType < 3){
+                newVersion = newVersion + it
+                releaseType ++
+            }
 
-    static String removeExtensionFrom( String name ) {
+        }
+
+        try{
+            Version.parseVersion(newVersion)
+            newVersion
+        }
+        catch (IllegalArgumentException e){
+            '1.0.0'
+        }
+    }
+
+    static String removeExtensionFrom(String name ) {
         def dot = name.lastIndexOf( '.' )
         if ( dot > 0 ) { // exclude extension
             return name[ 0..<dot ]
